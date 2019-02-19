7a9f030ef43e2443f714ed9d72f80ad3b47526d3
altazur <altazur.91@gmail.com>
2019-02-19 22:53:36 +0200

    1. Reading\writing text file ("last_message_number" inside the resources) instead of using the "magical number" which meant to be rewritten after every test launch

--- src/main/java/com/qatestlab/facebook/MainPageMap.java
+++ src/main/java/com/qatestlab/facebook/MainPageMap.java
@@ -6,11 +6,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class MainPageMap {
 
     private WebDriver driver;
+    private File file;
     //Magic Number
-    int MessageNumber = 10;
 
 
     public MainPageMap(WebDriver driver){
@@ -38,8 +44,17 @@ public class MainPageMap {
                 "span/div/div/div[2]/div/div/div"));
     }
 
-    public String LastMessage(){
-        return this.driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[1]/div/div/div[4]/div/div[1]/div/div/div/div/div/div[1]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div["+MessageNumber+"]/div/div/div/div[1]/div/div/div/div[2]/div/div/span/span")).getText();
+    public String LastMessage() throws IOException {
+        file = new File("src/main/resources/last_message_number");//text file for storing last message div number to increment it after
+        Scanner reader = new Scanner(file);//for reading the number
+        //reading number from file
+        int MessageNumber = reader.nextInt();
+        reader.close();//clean the reader
+        WebElement lastMessage = this.driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[1]/div/div/div[4]/div/div[1]/div/div/div/div/div/div[1]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div["+MessageNumber+++"]/div/div/div/div[1]/div/div/div/div[2]/div/div/span/span"));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));//creating object for write the new MessageNumber
+        writer.write(MessageNumber);//write
+        writer.close();//closing the file
+        return lastMessage.getText();//return the Last message text
     }
 
     public WebElement SendMessageButton(){
